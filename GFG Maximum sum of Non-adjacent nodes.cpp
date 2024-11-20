910cce2267ee48aa3c61224986a8e2ce2eb9a616
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2024-11-21 00:00:17 +0530

    Create GFG Maximum sum of Non-adjacent nodes.cpp

---
+++ GFG Maximum sum of Non-adjacent nodes.cpp
@@ -0,0 +1,24 @@
+class Solution{
+  public:
+    //Function to return the maximum sum of non-adjacent nodes.
+    
+    pair<int,int> solve(Node* root){
+        if(root==NULL){
+            pair<int,int>p=make_pair(0,0);
+            return p;
+        }
+        pair<int,int> left = solve(root->left);
+        pair<int,int> right = solve(root->right);
+        pair<int,int> result;
+        result.first = root->data + left.second + right.second;
+        result.second = max(left.first,left.second)+max(right.first,right.second);
+        return result;
+        
+    }
+    
+    int getMaxSum(Node *root) 
+    {
+        pair<int,int> ans = solve(root);
+        return max(ans.first,ans.second);
+    }
+};
